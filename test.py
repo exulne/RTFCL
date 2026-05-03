138c6923b85885192db6119b5ca1bc88d2763337
exulne <exulne@gmail.com>
2026-05-03 18:40:38 -0400

    addede req.txt, removed test.py

--- test.py
+++
@@ -1,9 +0,0 @@
-"https://youtu.be/qtyy95Vevrk"
-"https://www.youtube.com/watch?v=qtyy95Vevrk"
-
-content = "https://www.youtube.com/watch?v=qtyy95Vevrk"
-i = content.find("?v=")
-print(i)
-content = "https://youtu.be/" + content[i+3:]
-
-print(content)
