2a884019a43841f40762652e63c23b2f2d63a3bf
exulne <exulned@gmail.com>
2026-05-19 15:58:18 -0400

    slight refactor to fix file copying not working

--- main.py
+++ main.py
@@ -13,38 +13,33 @@ keys = list(forDiscord.keys())
 enabled_list = ["yt", "replace"]
 
 def RTFCL(content):
-    if content.startswith("http"):
-        print("detected: website")
+    # specific youtube check
+    if "youtube.com" in content:
+        print("detected: youtube.com")
+        i = content.find("?v=")
+        if i != -1:
+            print("detected: youtube video")
+            content = "https://youtu.be/" + content[i+3:]
+    
+    # general discord replacements
+    elif any(key in content for key in keys):
+        for key in keys:
+            if key in content:
+                replacementSite = forDiscord[key]
+                print(f"detected: {key}")
+                i = content.find(key)
 
-        # specific youtube check
-        if "youtube.com" in content:
-            print("detected: youtube.com")
-            i = content.find("?v=")
-            if i != -1:
-                print("detected: youtube video")
-                content = "https://youtu.be/" + content[i+3:]
-        
-        # general discord replacements
-        elif any(key in content for key in keys):
-            for key in keys:
-                if key in content:
-                    replacementSite = forDiscord[key]
-                    print(f"detected: {key}")
-                    i = content.find(key)
+                content = content[:i] + replacementSite + content[i+len(key):]
+                break
 
-                    content = content[:i] + replacementSite + content[i+len(key):]
-                    break
+    print("detected: other link")
+    if not content.lower().split("?")[0].endswith(image_extensions):
+            return content.split("?")[0]
 
-        print("detected: other link")
-        if not content.lower().split("?")[0].endswith(image_extensions):
-                return content.split("?")[0]
-    else:
-        print("not a website, nothing to clear")
     return content
 
 def main():
     print("Monitoring clipboard with pyclip...")
-    
     try:
         last_seen = pyclip.paste(text=True)
     except:
@@ -54,26 +49,25 @@ def main():
         try:
             raw_data = pyclip.paste()
             current_data = pyclip.paste(text=True)
-            is_html_image = current_data.strip().startswith(("<meta", "<img"))
 
-            if current_data != last_seen and current_data.strip() and not is_html_image:
-                print(f"Detected: {current_data[:30]}...")
+            #checks
+            is_img = current_data.strip().startswith(("<meta", "<img"))
+            is_website = current_data.strip().startswith("http")
+            is_file = current_data.strip().startswith("file://")
 
+            if current_data != last_seen and current_data.strip() and not is_img and not is_file:
+                print(f"Detected Something")
                 new_data = RTFCL(current_data.strip())
-
                 pyclip.copy(new_data)
 
                 last_seen = new_data
-                print("Clipboard updated!")
-            
-            elif is_html_image:
-                last_seen = raw_data
-
+                print("updated")
+            else:
+                last_seen = current_data
         except Exception as e:
             print(f"Error reading clipboard: {e}")
 
         time.sleep(0.2)
-
 
 if __name__ == "__main__":
     main()
---
+++ test.py
@@ -0,0 +1,6 @@
+import pyclip
+import time
+
+while True:
+    print(pyclip.paste())
+    print(pyclip.paste(text=True))
